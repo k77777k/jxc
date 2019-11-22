d57170355143ecc29e29703a9053e4c492be20db
easy no 6 <865273937@qq.com>
2019-11-22 15:05:13 +0800

    swagger测试2.0

--- src/main/java/com/wf/controller/TestController.java
+++ src/main/java/com/wf/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.wf.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Api(tags = {"为了测试Swagger"})
 public class TestController {
 
     @RequestMapping("/test")
+    @ApiOperation(value = "测试Swagger的方法")
     public String test(String echo){
 
         return "String"+echo;
